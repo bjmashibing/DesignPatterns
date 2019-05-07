e6c8ca740a0e054a1bdc49822bf827127bfcbda0
bjmashibing <alanpppbox@gmail.com>
2019-05-07 22:10:16 +0800

    设计模式之Singleton和Strategy

---
+++ src/com/mashibing/strategy/Comparable.java
@@ -0,0 +1,5 @@
+package com.mashibing.strategy;
+
+public interface Comparable<T> {
+    int compareTo(T o);
+}
---
+++ src/com/mashibing/strategy/Comparator.java
@@ -0,0 +1,5 @@
+package com.mashibing.strategy;
+
+public interface Comparator<T> {
+    int compare(T o1, T o2);
+}
