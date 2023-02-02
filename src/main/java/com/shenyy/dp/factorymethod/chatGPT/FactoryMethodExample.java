d6a6112681de5af1e4836b5fe58a11ae7ba7d0da
shenyy <shenyy@eav.top>
2023-02-02 18:09:41 +0800

    工厂模式示例

--- src/main/java/com/shenyy/dp/factorymethod/chatGPT/FactoryMethodExample.java
+++ src/main/java/com/shenyy/dp/factorymethod/chatGPT/FactoryMethodExample.java
@@ -4,5 +4,7 @@ public class FactoryMethodExample {
     public static void main(String[] args) {
         Dessert dessert = new DessertFactory().getDessert("Cake");
         dessert.make();
+        Dessert dessert1 = new DessertFactory().getDessert("Pie");
+        dessert1.make();
     }
 }
